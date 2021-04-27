cf22201025f78340bb6a583eb4482dc57400fad2
jarranzen <jarran.a.zen@gmail.com>
2021-04-27 15:02:20 +0930

    Add files via upload

---
+++ Hello_World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aFriend = "Bill";
+
+            Console.WriteLine("Hello " + aFriend);
+        }
+    }
+}
