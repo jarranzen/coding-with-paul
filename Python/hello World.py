c0a4e30b0f381d4ff73a682b31cd7c8ab40865c3
jarranzen <jarran.a.zen@gmail.com>
2021-05-17 11:10:00 +0930

    Add files via upload

---
+++ Python/hello World.py
@@ -0,0 +1,23 @@
+Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello World')
+Hello World
+>>> len('hello')
+5
+>>> str(436)
+'436'
+>>> int(five)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    int(five)
+NameError: name 'five' is not defined
+>>> float(3.445)
+3.445
+>>> len int  (24357)
+SyntaxError: invalid syntax
+>>> 
+============================================================ RESTART: Shell ============================================================
+>>> print('Hello World')
+Hello World
+>>> 
